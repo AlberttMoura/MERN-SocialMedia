a3374aa80fb65acc7d2310b46da41cc7d078dff6
Alberto Moura <56458241+AlberttMoura@users.noreply.github.com>
2021-08-15 10:59:09 -0300

    Barra lateral direita feita

--- client/src/components/rightsidebar/Rightsidebar.jsx
+++ client/src/components/rightsidebar/Rightsidebar.jsx
@@ -3,7 +3,58 @@ import "./rightsidebar.css"
 function Rightsidebar() {
     return (
         <div className="rightsidebar">
-            rightsidebar
+            <div className="rightsidebarWrapper">
+                <div className="birthdayContainer">
+                    <img className="birthdayImg" src="/assets/general/gift.png" alt="" />
+                    <span className="birthdayText"><b>Albert Eintein</b> e <b>outros 2 amigos</b> fazem aniversário hoje</span>
+                </div>
+                <img src="/assets/general/fiatad.jpg" alt="" className="rightsidebarAd" />
+                <h4 className="rightsidebarTitle">Online Friends</h4>
+                <ul className="rightsidebarFriendList">
+                    <li className="rightsidebarFriend">
+                        <div className="rightsidebarProfileImgContainer">
+                            <img src="/assets/person/person3.jpg" alt="" className="rightsidebarProfileImg" />
+                            <span className="rightsidebarOnline"></span>
+                        </div>
+                        <span className="rightsidebarUsername">Albert Einstein</span>
+                    </li>
+                    <li className="rightsidebarFriend">
+                        <div className="rightsidebarProfileImgContainer">
+                            <img src="/assets/person/person3.jpg" alt="" className="rightsidebarProfileImg" />
+                            <span className="rightsidebarOnline"></span>
+                        </div>
+                        <span className="rightsidebarUsername">Albert Einstein</span>
+                    </li>
+                    <li className="rightsidebarFriend">
+                        <div className="rightsidebarProfileImgContainer">
+                            <img src="/assets/person/person3.jpg" alt="" className="rightsidebarProfileImg" />
+                            <span className="rightsidebarOnline"></span>
+                        </div>
+                        <span className="rightsidebarUsername">Albert Einstein</span>
+                    </li>
+                    <li className="rightsidebarFriend">
+                        <div className="rightsidebarProfileImgContainer">
+                            <img src="/assets/person/person3.jpg" alt="" className="rightsidebarProfileImg" />
+                            <span className="rightsidebarOnline"></span>
+                        </div>
+                        <span className="rightsidebarUsername">Albert Einstein</span>
+                    </li>
+                    <li className="rightsidebarFriend">
+                        <div className="rightsidebarProfileImgContainer">
+                            <img src="/assets/person/person3.jpg" alt="" className="rightsidebarProfileImg" />
+                            <span className="rightsidebarOnline"></span>
+                        </div>
+                        <span className="rightsidebarUsername">Albert Einstein</span>
+                    </li>
+                    <li className="rightsidebarFriend">
+                        <div className="rightsidebarProfileImgContainer">
+                            <img src="/assets/person/person3.jpg" alt="" className="rightsidebarProfileImg" />
+                            <span className="rightsidebarOnline"></span>
+                        </div>
+                        <span className="rightsidebarUsername">Albert Einstein</span>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
